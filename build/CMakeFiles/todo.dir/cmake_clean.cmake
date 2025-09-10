0ec54bab9b0cd9e144f3c5024190da9b394af8fc
ducndc <nguyendangchungduc1999@gmail.com>
2025-09-10 19:44:39 +0700

    add cmake

---
+++ build/CMakeFiles/todo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/todo.dir/main.cc.o"
+  "CMakeFiles/todo.dir/main.cc.o.d"
+  "CMakeFiles/todo.dir/todo_app.cc.o"
+  "CMakeFiles/todo.dir/todo_app.cc.o.d"
+  "todo"
+  "todo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/todo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
